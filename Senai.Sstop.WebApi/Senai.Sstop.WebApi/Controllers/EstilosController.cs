3ccbe72aa92e7e606867bf36010f0735233f6563
Cleís Aurora Silveira Pereira <34132336883@info.local>
2019-08-19 11:18:13 -0300

    Primeiro Commit

---
+++ Senai.Sstop.WebApi/Senai.Sstop.WebApi/Controllers/EstilosController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Sstop.WebApi.Domains;
+using Senai.Sstop.WebApi.Repositories;
+
+namespace Senai.Sstop.WebApi.Controllers
+{
+    [Route("api/estilos")]
+    [Produces("application/json")]
+    [ApiController]
+   
+    public class EstilosController : ControllerBase
+    {
+        List<EstiloDomain> estilos = new List<EstiloDomain>()
+        {
+            new EstiloDomain { IdEstilo = 1, Nome = "Rock"}
+            , new EstiloDomain { IdEstilo = 1, Nome = "Pop"}
+        };
+
+        EstiloRepository estiloRepository = new EstiloRepository();
+
+        [Produces("application/json")]
+
+        [HttpGet]
+        public IEnumerable<EstiloDomain> Listar()
+        {
+            //return estilos;
+            return estiloRepository.Listar();
+        }
+
+        
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+           
+
+            EstiloDomain Estilo = estilos.Find(x => x.IdEstilo == id);
+            if(Estilo == null)
+            {
+                return NotFound();
+            }
+            return Ok(Estilo);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(EstiloDomain estiloDomain)
+        {
+            estilos.Add(new EstiloDomain
+            {
+                IdEstilo = estilos.Count + 1,
+                Nome = estiloDomain.Nome
+            }
+            );
+            return Ok(estilos);
+        }
+    }
+}
---
+++ Senai.Sstop.WebApi/Senai.Sstop.WebApi/Repositories/EstiloRepository.cs
@@ -0,0 +1,60 @@
+﻿using Senai.Sstop.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Sstop.WebApi.Repositories
+{
+    public class EstiloRepository
+    {
+        //List<EstiloDomain> estilos = new List<EstiloDomain>()
+        //{
+        //    new EstiloDomain { IdEstilo = 1, Nome = "Rock"}
+        //   , new EstiloDomain { IdEstilo = 1, Nome = "Pop"}
+        //};
+
+        
+
+        private string StringConexao = "Data Source =.\\SqlExpress; initial catalog=M_SStop;User Id = sa;Pwd=132";
+
+        public List<EstiloDomain> Listar()
+        {
+
+            List<EstiloDomain> estilos = new List<EstiloDomain>();
+
+            //abre conexao com banco
+            using (SqlConnection con = new SqlConnection(StringConexao))
+            {
+                //faz leitura com todos os registros
+                // declara a instrucao a ser realizada
+                string Query = "SELECT IdEstiloMusical, Nome FROM EstilosMusicas order by asc";
+
+                //abre a conexão com bd
+                con.Open();
+
+                //declara para percorrer a lista
+                SqlDataReader rdr;
+
+                using (SqlCommand cmd = new SqlCommand(Query, con))
+                {
+                    //executa a query
+                    rdr = cmd.ExecuteReader();
+
+                    while(rdr.Read())
+                    {
+                        EstiloDomain estilo = new EstiloDomain
+                        {
+                            IdEstilo = Convert.ToInt32(rdr["IdEstiloMusical"]),
+                            Nome = rdr["Nome"].ToString()
+                        };
+                        estilos.Add(estilo);
+                    }
+                }
+            }
+
+                return estilos;
+        }
+    }
+}
